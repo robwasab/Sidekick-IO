88e0deaa65c539735e748bb4c7f08a87705c0986
Robby Tong <robbytong@gmail.com>
2021-01-10 12:14:07 -0800

    boilder plate bootloader code for starting a minimal project

---
+++ Bootloader/Bootloader/src/main.c
@@ -0,0 +1,20 @@
+#include <asf.h>
+
+int main (void)
+{
+	system_init();
+
+	/* Insert application code here, after the board has been initialized. */
+
+	/* This skeleton code simply sets the LED to the state of the button. */
+	while (1) {
+		/* Is button pressed? */
+		if (port_pin_get_input_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE) {
+			/* Yes, so turn LED on. */
+			port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+		} else {
+			/* No, so turn LED off. */
+			port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		}
+	}
+}
