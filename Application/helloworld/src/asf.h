ca41e98b6a2e611913eaac5d41518c5ab484e530
Robby Tong <robbytong@gmail.com>
2021-02-14 18:05:26 -0800

    asf includes

--- Application/helloworld/src/asf.h
+++ Application/helloworld/src/asf.h
@@ -56,6 +56,16 @@
 #include <compiler.h>
 #include <status_codes.h>
 
+// From module: DMAC - Direct Memory Access Controller
+#include <dma.h>
+#include <dma_crc.h>
+
+// From module: EVSYS - Event System Common
+#include <events.h>
+
+// From module: EVSYS - Event System with interupt hooks support
+#include <events_hooks.h>
+
 // From module: EXTINT - External Interrupt (Callback APIs)
 #include <extint.h>
 #include <extint_callback.h>
